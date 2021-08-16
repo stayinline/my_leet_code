6068d241ebc3e470f8b3f0ce3c82ca1d4b7084e8
hemaoling <hemaoling3@xdf.cn>
2021-08-16 10:42:26 +0800

    日拱一卒

---
+++ src/leetcode/Case380.java
@@ -0,0 +1,60 @@
+package leetcode;
+
+import java.util.*;
+
+public class Case380 {
+
+    class RandomizedSet {
+
+        private List<Integer> values;
+        private Map<Integer, Integer> map;
+
+        /**
+         * Initialize your data structure here.
+         */
+        public RandomizedSet() {
+            values = new ArrayList<>();
+            map = new HashMap<>();
+        }
+
+        /**
+         * Inserts a value to the set. Returns true if the set did not already contain the specified element.
+         */
+        public boolean insert(int val) {
+            if (map.get(val) != null) {
+                return false;
+            }
+
+            map.put(val, values.size());
+            values.add(values.size(), val);
+            return true;
+        }
+
+        /**
+         * Removes a value from the set. Returns true if the set contained the specified element.
+         */
+        public boolean remove(int val) {
+            if (map.get(val) == null) {
+                return false;
+            }
+            int tailVal = values.get(values.size() - 1);
+            int idxOfVal = map.get(val);
+
+            map.put(tailVal, idxOfVal);
+            values.set(idxOfVal, tailVal);
+
+            values.remove(values.size() - 1);
+            map.remove(val);
+            return true;
+        }
+
+        /**
+         * Get a random element from the set.
+         */
+        public int getRandom() {
+            int index = new Random().nextInt(values.size());
+            return values.get(index);
+        }
+
+    }
+}
