678c9e4d71cb56357c141d70d3f056d7f2e64964
hemaoling <hemaoling3@xdf.cn>
2021-09-01 19:00:17 +0800

    日拱一卒

---
+++ src/leetcode/Case26.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class Case26 {
+
+    public static int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int j = i + 1; j < nums.length;j++) {
+            if (nums[i] != nums[j]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 1, 2};
+        removeDuplicates(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
