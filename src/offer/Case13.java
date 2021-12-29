1a5e6e9682e8a2856ddb00f57970b3da1dd9e44d
hemaoling <hemaoling3@xdf.cn>
2021-12-29 19:28:07 +0800

    日拱一卒

--- src/offer/Case13.java
+++ src/offer/Case13.java
@@ -2,9 +2,9 @@ package offer;
 
 public class Case13 {
 
-    class Solution {
+    static class Solution {
         int m, n, k;
-        boolean[][] visited;
+        boolean[][] visited; // 记录哪些i、j被访问过了
 
         public int movingCount(int m, int n, int k) {
             this.m = m;
@@ -15,8 +15,8 @@ public class Case13 {
         }
 
         /**
-         * @param i 行索引
-         * @param j 列索引
+         * @param i  行索引
+         * @param j  列索引
          * @param si
          * @param sj
          * @return
@@ -26,7 +26,12 @@ public class Case13 {
                 return 0;
             }
             visited[i][j] = true;
-            return 1 + dfs(i + 1, j, (i + 1) % 10 != 0 ? si + 1 : si - 8, sj) + dfs(i, j + 1, si, (j + 1) % 10 != 0 ? sj + 1 : sj - 8);
+            return 1 + dfs(i + 1, j, (i + 1) % 10 != 0 ? si + 1 : si - 8, sj)
+                    + dfs(i, j + 1, si, (j + 1) % 10 != 0 ? sj + 1 : sj - 8);
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Case13.Solution().movingCount(2, 3, 1));
     }
 }
