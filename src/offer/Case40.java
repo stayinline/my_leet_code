af54cc72b4258a9eefc3c108d570ef6d4adb516f
hemaoling <hemaoling3@xdf.cn>
2022-03-16 00:23:50 +0800

    top k

--- src/offer/Case40.java
+++ src/offer/Case40.java
@@ -5,16 +5,41 @@ import java.util.PriorityQueue;
 
 public class Case40 {
 
+//    public static int[] getLeastNumbers(int[] arr, int k) {
+//        int[] ret = new int[k];
+//        if (k == 0) {
+//            return ret;
+//        }
+//        if (arr.length <= k) {
+//            return arr;
+//        }
+//
+//
+//        PriorityQueue<Integer> q = new PriorityQueue<>((x1, x2) -> x2 - x1);
+//        for (int i = 0; i < k; i++) {
+//            q.offer(arr[i]);
+//        }
+//
+//        for (int i = k; i < arr.length; i++) {
+//            if (q.peek() > arr[i]) {
+//                q.poll();
+//                q.offer(arr[i]);
+//            }
+//        }
+//
+//        for (int i = 0; i < k; i++) {
+//            ret[i] = q.poll();
+//        }
+//        return ret;
+//    }
+
+
     public static int[] getLeastNumbers(int[] arr, int k) {
         int[] ret = new int[k];
-        if (k == 0) {
+
+        if (arr.length <= 0 || k <= 0) {
             return ret;
         }
-        if (arr.length <= k) {
-            return arr;
-        }
-
-
         PriorityQueue<Integer> q = new PriorityQueue<>((x1, x2) -> x2 - x1);
         for (int i = 0; i < k; i++) {
             q.offer(arr[i]);
@@ -34,8 +59,8 @@ public class Case40 {
     }
 
     public static void main(String[] args) {
-        int[] arr = {0, 0, 0, 2, 0, 5};
-        int k = 0;
+        int[] arr = {3, 2, 1};
+        int k = 2;
         System.out.println(Arrays.toString(getLeastNumbers(arr, k)));
     }
 }
