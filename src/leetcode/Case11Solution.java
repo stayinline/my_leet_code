cc44900f4d629eeb5c91a600d72d38bb9a4cf5c1
hemaoling <hemaoling3@xdf.cn>
2022-04-27 10:16:44 +0800

    每日一题

--- src/leetcode/Case11Solution.java
+++ src/leetcode/Case11Solution.java
@@ -5,8 +5,10 @@ public class Case11Solution {
         int l = 0, r = height.length - 1;
         int ans = 0;
         while (l < r) {
+            // 木桶理论短板效应
             int area = Math.min(height[l], height[r]) * (r - l);
             ans = Math.max(ans, area);
+            // 经典二分法的思想来挪动指针
             if (height[l] < height[r]) {
                 ++l;
             } else {
