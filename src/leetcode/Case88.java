856032ab2af24a73d9325b8b90bad9dc96fa64c1
hemaoling <hemaoling3@xdf.cn>
2023-10-30 15:57:44 +0800

    每日一题

--- src/leetcode/Case88.java
+++ src/leetcode/Case88.java
@@ -11,11 +11,11 @@ public class Case88 {
         Arrays.sort(nums1);
     }
 
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    public void mergeOfficial(int[] nums1, int m, int[] nums2, int n) {
         int p1 = 0, p2 = 0;
         int[] sorted = new int[m + n];
-        int cur;
         while (p1 < m || p2 < n) {
+            int cur;
             if (p1 == m) {
                 cur = nums2[p2++];
             } else if (p2 == n) {
@@ -27,8 +27,43 @@ public class Case88 {
             }
             sorted[p1 + p2 - 1] = cur;
         }
-        for (int i = 0; i != m + n; ++i) {
+        for (int i = 0; i != m + n; i++) {
             nums1[i] = sorted[i];
+        }
+    }
+
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] tmp = new int[m + n];
+        int i = 0, j = 0, k = 0;
+        for (; i < m && j < n; k++) {
+            if (nums1[i] <= nums2[j]) {
+                tmp[k] = nums1[i];
+                i++;
+
+            } else if (nums1[i] > nums2[j]) {
+                tmp[k] = nums2[j];
+                j++;
+            }
+
+        }
+
+        if (i >= m) {
+            while (j < n) {
+                tmp[k] = nums2[j];
+                j++;
+                k++;
+            }
+        } else {
+            while (i < m) {
+                tmp[k] = nums1[i];
+                i++;
+                k++;
+            }
+        }
+
+
+        for (int t = 0; t < m + n; t++) {
+            nums1[t] = tmp[t];
         }
     }
 
