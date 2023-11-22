09644e6d1624d83704f1fc792c0ebba2a69e14a6
hemaoling <hemaoling3@xdf.cn>
2023-11-22 11:40:40 +0800

    每日一题

--- src/leetcode/Case36.java
+++ src/leetcode/Case36.java
@@ -1,11 +1,12 @@
 package leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Case36 {
 
-    public boolean isValidSudoku(char[][] board) {
-        // key:num  value:count of num 存数数据类似于： rows[0]={[5:1],[2:1]}
+    public static boolean isValidSudoku(char[][] board) {
+        // key:当前元素  value:当前元素出现的次数
         HashMap<Integer, Integer>[] rows = new HashMap[9];
         HashMap<Integer, Integer>[] columns = new HashMap[9];
         HashMap<Integer, Integer>[] boxes = new HashMap[9];
@@ -15,17 +16,21 @@ public class Case36 {
             boxes[i] = new HashMap<>();
         }
 
+        // 横着的
         for (int i = 0; i < 9; i++) {
+            // 竖着的
             for (int j = 0; j < 9; j++) {
                 char num = board[i][j];
                 if (num != '.') {
-                    int n = (int) num;
+                    int n = num;
+                    // 计算当前元素在小九格的下标位置
                     int box_index = (i / 3) * 3 + j / 3;
 
                     rows[i].put(n, rows[i].getOrDefault(n, 0) + 1);
                     columns[j].put(n, columns[j].getOrDefault(n, 0) + 1);
                     boxes[box_index].put(n, boxes[box_index].getOrDefault(n, 0) + 1);
 
+                    // 只要当前元素，在任何一个map中出现的次数超过了1次，就无效
                     if (rows[i].get(n) > 1 || columns[j].get(n) > 1 || boxes[box_index].get(n) > 1)
                         return false;
                 }
@@ -33,6 +38,20 @@ public class Case36 {
         }
 
         return true;
+    }
+
+    public static void main(String[] args) {
+        HashMap<Integer, Integer>[] rows = new HashMap[9];
+        HashMap<Integer, Integer>[] columns = new HashMap[9];
+        HashMap<Integer, Integer>[] boxes = new HashMap[9];
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashMap<>();
+            columns[i] = new HashMap<>();
+            boxes[i] = new HashMap<>();
+        }
+        System.out.println(Arrays.toString(rows));
+        System.out.println(Arrays.toString(columns));
+        System.out.println(Arrays.toString(boxes));
     }
 
 }
