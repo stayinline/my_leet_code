b6d4d4cf1cfc874775493ea763d8e0e5f6658930
hemaoling <hemaoling3@xdf.cn>
2024-04-23 16:04:44 +0800

    每日一题

--- src/leetcode/Case73.java
+++ src/leetcode/Case73.java
@@ -4,24 +4,45 @@ import java.util.Arrays;
 
 public class Case73 {
 
+    // 注意审题：是将0所在的行和列全部设置为0，而不是将0的上下左右设置为0
+
+//    public static void setZeroesError(int[][] matrix) {
+//        int m = matrix.length;
+//        int n = matrix[0].length;
+//        int[] rows = new int[m];
+//        int[] cols = new int[n];
+//
+//        for (int i = 0; i < m; i++) {
+//            for (int j = i; j < n; j++) {
+//                if (matrix[i][j] == 0 && rows[i] == 0 && cols[j] == 0) {
+//                    rows[i] = 1;
+//                    cols[j] = 1;
+//                    for (int a = 0; a < m; a++) {
+//                        matrix[a][j] = 0;
+//                    }
+//                    for (int a = 0; a < n; a++) {
+//                        matrix[i][a] = 0;
+//                    }
+//                }
+//            }
+//        }
+//    }
 
     public static void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int[] rows = new int[m];
-        int[] cols = new int[n];
-
+        int m = matrix.length, n = matrix[0].length;
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
         for (int i = 0; i < m; i++) {
-            for (int j = i; j < n; j++) {
-                if (matrix[i][j] == 0 && rows[i] == 0 && cols[j] == 0) {
-                    rows[i] = 1;
-                    cols[j] = 1;
-                    for (int a = 0; a < m; a++) {
-                        matrix[a][j] = 0;
-                    }
-                    for (int a = 0; a < n; a++) {
-                        matrix[i][a] = 0;
-                    }
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = col[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
                 }
             }
         }
